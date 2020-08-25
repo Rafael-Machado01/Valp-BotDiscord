611772c42287b58bfa82e0e9bb7125b89a080f7d
Rafael-Machado01 <70187787+Rafael-Machado01@users.noreply.github.com>
2020-08-25 01:19:05 -0300

    Delete guildDelete.js

--- events/guildDelete.js
+++
@@ -1,7 +0,0 @@
-module.exports = (client, guild) => {
-    client.logger.cmd(`[GUILD LEAVE] ${guild.name} (${guild.id}) Removeu o bot.`);
-  
-    if (client.settings.has(guild.id)) {
-      client.settings.delete(guild.id);
-    }
-  };
