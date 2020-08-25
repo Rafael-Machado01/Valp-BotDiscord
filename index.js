badd2ec31a7064b1b8ea1a82fd01b48bb6bfc788
Rafael-Machado01 <70187787+Rafael-Machado01@users.noreply.github.com>
2020-08-25 01:14:41 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,76 @@
+const Discord = require("discord.js");
+const Enmap = require("enmap");
+const fs = require("fs");
+const client = new Discord.Client();
+const config = require("./config.json");
+client.config = config;
+
+fs.readdir("./events/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    const event = require(`./events/${file}`);
+    let eventName = file.split(".")[0];
+    client.on(eventName, event.bind(null, client));
+  });
+});
+
+client.commands = new Enmap();
+
+fs.readdir("./commands/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    if (!file.endsWith(".js")) return;
+    let props = require(`./commands/${file}`);
+    let commandName = file.split(".")[0];
+    console.log(`carregando ${commandName}`);
+    client.commands.set(commandName, props);
+  });
+});
+
+
+client.on("guildMemberAdd", async (member) => { 
+
+  let guild = await client.guilds.cache.get("725073325725974568   ");
+  let channel = await client.channels.cache.get("726560371938426930");
+  let emoji = await member.guild.emojis.cache.find(emoji => emoji.name === "nomedoemoji");
+  if (guild != member.guild) {
+    return console.log("Sem boas-vindas pra você! Sai daqui saco pela.");
+   } else {
+      let embed = await new Discord.MessageEmbed()
+      .setColor("#7c2ae8")
+      .setAuthor(member.user.tag, member.user.displayAvatarURL())
+      .setTitle(`${emoji} Boas-vindas ${emoji}`)
+      .setDescription(`**${member.user}**, bem-vindo(a) ao servidor **${guild.name}**! Atualmente estamos com **${member.guild.memberCount} membros**, divirta-se conosco! :heart:`)
+      .setThumbnail(member.user.displayAvatarURL({ dynamic: true, format: "png", size: 1024 }))
+      .setFooter("ValpBOT")
+      .setTimestamp();
+
+    channel.send(embed);
+  }
+});
+
+
+
+
+
+client.on("ready", () => {
+  let activities = [
+      `Utilize ${config.prefix}help para obter ajuda`,
+      `Estou em ${client.guilds.cache.size} servidores!`,
+      `Estou visivel em ${client.channels.cache.size} canais!`,
+      `Adiminstrando para ${client.users.cache.size} usuários!`
+    ],
+    i = 0;
+  setInterval( () => client.user.setActivity(`${activities[i++ % activities.length]}`, {
+        type: "WATCHING"
+      }), 1000 * 60); 
+  client.user
+      .setStatus("dnd")
+      .catch(console.error);
+console.log("Estou Online! Banco de dados UN ON-log")
+
+
+
+
+});
+client.login(config.token);
